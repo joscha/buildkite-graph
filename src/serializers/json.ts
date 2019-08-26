1223aae553bb795721349b607696ecd48d06a3d3
Joscha Feth <joscha@canva.com>
2019-08-26 11:47:55 +1000

    feat: 🎸 JSON serializer can have stringified output

--- src/serializers/json.ts
+++ src/serializers/json.ts
@@ -2,9 +2,12 @@ import { classToPlain } from 'class-transformer';
 import { Pipeline } from '../';
 import { Serializer } from '.';
 
-export class JsonSerializer implements Serializer<object> {
-    serialize(e: Pipeline): object {
+export class JsonSerializer implements Serializer<object | string> {
+    constructor(private readonly stringify: boolean = false) {}
+
+    serialize(e: Pipeline): object | string {
         // Workaround to get rid of undefined values
-        return JSON.parse(JSON.stringify(classToPlain(e)));
+        const json = JSON.parse(JSON.stringify(classToPlain(e)));
+        return this.stringify ? JSON.stringify(json) : json;
     }
 }
