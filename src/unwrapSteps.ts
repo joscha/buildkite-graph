253c63f2a6e7582ab976aed16befbb88d3e4c073
Joscha Feth <joscha@canva.com>
2019-12-03 16:12:13 +1100

    refactor: 💡 simplify condition

--- src/unwrapSteps.ts
+++ src/unwrapSteps.ts
@@ -10,15 +10,12 @@ export async function unwrapSteps(
     const ret: Step[] = [];
     for (const s of steps) {
         if (s instanceof Conditional) {
-            if ((await s.accept()) === true || cache.has(s)) {
-                let cond: Step;
-                if (cache.has(s)) {
-                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                    cond = cache.get(s)!;
-                } else {
-                    cond = await s.get();
-                    cache.set(s, cond);
-                }
+            if (cache.has(s)) {
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                ret.push(cache.get(s)!);
+            } else if ((await s.accept()) === true) {
+                const cond = await s.get();
+                cache.set(s, cond);
                 ret.push(cond);
             }
         } else {
